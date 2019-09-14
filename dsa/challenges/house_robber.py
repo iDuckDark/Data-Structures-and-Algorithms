ad1e950a34d1c93b406cb4a4b8e986169a2810fb
Ben Burk <benburk@outlook.com>
2019-09-14 17:16:22 -0400

    add house robber challenge

---
+++ dsa/challenges/house_robber.py
@@ -0,0 +1,26 @@
+"""
+Rob houses
+
+References:
+- https://leetcode.com/problems/house-robber/
+"""
+
+from typing import List
+
+
+def rob(nums: List[int]) -> int:
+    """
+    O(n)
+    """
+    prev1, prev2 = 0, 0
+    for num in nums:
+        prev1, prev2 = max(prev2 + num, prev1), prev1
+
+    return prev1
+
+
+def test():
+    """test"""
+    test_cases = (([1, 2, 3, 1], 4), ([2, 7, 9, 3, 1], 12))
+    for arg, result in test_cases:
+        assert rob(arg) == result
