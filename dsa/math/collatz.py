b4cf28d2f0ab4a51abaff368fd790bfad8f29181
Ben Burk <benburk@outlook.com>
2019-09-14 12:52:12 -0400

    remove unnecessary elif

--- dsa/math/collatz.py
+++ dsa/math/collatz.py
@@ -12,7 +12,7 @@ def collatz(number):
     while number != 1:
         if number % 2 == 0:
             number //= 2
-        elif number % 2 == 1:
+        else:
             number = 3 * number + 1
         result.append(number)
     return result
